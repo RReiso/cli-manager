bcce92595c0b3527a3695f355c39ea50984bb755
Ruta Reisoglu <rutareiso@protonmail.com>
2022-12-02 22:13:51 -0500

    Add welcome message

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from menu_items import MENU_ITEMS
 from menu import Menu
+from colors import bcolors
 
+print(f"{bcolors.YELLOW}Welcome to your personal manager app!\n")
 menu = Menu()
 menu.run(MENU_ITEMS)
